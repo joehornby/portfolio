ea2b533a5ec82e287261de11441e3491d8c7b623
Joseph Hornby <joehornby@gmail.com>
2020-07-28 01:16:16 +0100

    Add primary button component

---
+++ components/PrimaryButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <button @click="callback($event)"><slot></slot></button>
+</template>
+
+<script>
+export default {
+    props: ['text'],
+    methods: {
+    callback: function(e) {
+      this.$emit('click', e);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    button {
+        color: $light-grey;
+        background-color: $dark-grey;
+        
+        width: auto;
+        font-size: inherit;
+        font-family: inherit;
+        border-radius: 10px;
+        border: none;
+        height: 2rem;
+        margin: 0;
+        padding: 0.25rem 1rem;
+
+        &:hover {
+            background-color: rgba($dark-grey,0.7);
+            cursor: pointer;
+        }
+    }
+</style>
