13b2d0c53b1aa3dd33c7e6d47c25ef42d3805e16
Joseph Hornby <joehornby@gmail.com>
2020-07-29 00:56:21 +0100

    Add project children

---
+++ pages/projects/_id.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        This is in project: {{ project.fields.title }}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['project']
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
