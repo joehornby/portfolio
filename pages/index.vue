a80487398d1ba33b7544c384f97bac2e77d23049
Joseph Hornby <joehornby@gmail.com>
2020-07-28 01:24:55 +0100

    Add CMS API call, remove default styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,39 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        portfolio
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="home">
+    <h1 class="headline">{{ home.fields.h1 }}</h1>
+    <h2 class="description">{{ home.fields.h2 }}</h2>
+    <section class="work">
+      <nuxt-link to="/projects">
+        <primary-button>{{ home.fields.cta }}</primary-button>
+      </nuxt-link>
+    </section>
   </div>
 </template>
 
 <script>
-export default {}
+  import {createClient} from '~/plugins/contentful.js'
+  import PrimaryButton from '~/components/PrimaryButton'
+
+  const client = createClient()
+
+  export default {
+    components: {
+      PrimaryButton
+    },
+    asyncData ({env}) {
+      return Promise.all([
+        client.getEntry('2fRUBWy1oY6L18nlGnKBxk')
+      ])
+      .then( (entry) => {
+        // console.log(entry)
+        return {
+          home: entry[0]
+        }
+      }).catch( console.error )
+    }
+  }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="scss">
+  @import url('@/assets/scss/_home.scss');
 </style>
