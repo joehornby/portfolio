d1fe16e23050c44cd607745bafaf5b49d820154b
Joseph Hornby <joehornby@gmail.com>
2020-09-03 17:56:43 +0100

    Add dist folder

---
+++ plugins/dist/vue-carousel.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _vue = _interopRequireDefault(require("vue"));
+
+var _vueCarousel = _interopRequireDefault(require("vue-carousel"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_vue["default"].use(_vueCarousel["default"]);
---
+++ plugins/dist/vue-img.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _vue = _interopRequireDefault(require("vue"));
+
+var _vImg = _interopRequireDefault(require("v-img"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_vue["default"].use(_vImg["default"]);
