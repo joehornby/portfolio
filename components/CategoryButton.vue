ced47c0fd50a1b9f36c7f2cd4e78a813f26fd6cc
Joseph Hornby <joehornby@gmail.com>
2020-10-25 11:26:42 +0000

    add project category filter

---
+++ components/CategoryButton.vue
@@ -0,0 +1,48 @@
+<template>
+    <button
+          class="category-btn"
+          :class="{ 'category-btn__selected' : selectedCategory === category }"
+          @click="$emit('update:selectedCategory', category)"
+          >
+            {{ category }}
+    </button>
+</template>
+
+<script>
+export default {
+  props: [
+    'category',
+    'selectedCategory'
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+  .category-btn {
+    box-shadow: none;
+    border: $dark-grey solid 1px;
+    border-radius: 5px;
+    padding: 0.75em 1.5em;
+    margin: 0 0.2rem 0.2rem 0;
+    background-color: $light-grey;
+    &:hover {
+      background-color: $mid-grey;
+      cursor: pointer;
+    }
+    &__selected {
+      background-color: $dark-grey;
+      color: $light-grey;
+      // &:hover {
+      //   background: lighten($dark-grey, 20);
+      // }
+    }
+  }
+
+  @media screen and (min-width: $bp-md) {
+    .category-btn {
+      padding: 0.25em 0.5em;
+    }
+  }
+
+  
+</style>
--- components/ProjectList.vue
+++ components/ProjectList.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="work">
-      <h2 class="work__heading" id="project-list">Selected Projects</h2>
+      <div class="work__heading">
+        <h2 id="project-list">Selected Projects</h2>
+        <h3 class="filter">Filter by Category</h3>
+        <CategoryButton 
+          v-for="cat in categories" 
+          :key="cat" :category="cat" 
+          :selectedCategory.sync="selectedCategory"
+          v-on:updateCategory="selectedCategory" />
+        
+      </div>
       <ul>
-        <li class="work__list" v-for="(project, index) in projects" :key="project.fields.id">
+        <li class="work__list" v-for="(project, index) in filteredProjects" :key="project.fields.id">
           <nuxt-link tabindex="0" :to="`/projects/${project.fields.slug}`" prefetch>
             <h3 class="work__title">
               {{ project.fields.title }}
@@ -11,16 +20,16 @@
           <p class="work__details">
               {{ project.fields.headline }}
           </p>
-            <ul class="work__categories">
-              <li v-for="category in projects[index].fields.categories" :key="category" class="work__category">
-                {{ category }}
-              </li>
-            </ul>
-            <video v-if="project.fields.video && index < 6 && displayVideos" :src="project.fields.video.fields.file.url" 
-                autoplay="true" 
-                muted="true" 
-                loop="true" 
-                controls />
+          <ul class="work__categories">
+            <li v-for="category in project.fields.categories" :key="category" class="work__category">
+              {{ category }}
+            </li>
+          </ul>
+          <video v-if="project.fields.video && index < 6 && displayVideos" :src="project.fields.video.fields.file.url" 
+              autoplay="true" 
+              muted="true" 
+              loop="true" 
+              controls />
           
         </li>
       </ul>
@@ -29,15 +38,40 @@
 
 <script>
 export default {
-  props: [
-    "projects"
-  ],
+  data() {
+    return {
+      selectedCategory: 'All',
+      categories: [
+        'All',
+        'Web',
+        'Development',
+        'Design',
+        'Art',
+        'Immersive Technology',
+        'Sound'
+      ]
+    }
+  },
+  mounted() {
+    this.selectedCategory = this.$route.query.category ? this.$route.$query.category : "All"
+  },
   computed: {
+    projects() {
+        return this.$store.state.content.projects
+    },
     displayVideos() {
       /* return true - display the videos - if we're not in small breakpoint */
-      return this.$mq !== 'sm'
+      return this.$mq !== "sm"
+    },
+    filteredProjects() {
+      if (this.selectedCategory === 'All'){
+        return this.projects
+      } else {
+        return this.projects.filter( p => p.fields.categories.includes(this.selectedCategory))
+      }
     }
-  }
+  },
+  transition: 'slide-bottom'
 }
 </script>
 
@@ -64,9 +98,7 @@ export default {
           } 
         }
     }
-    &__title {
-      font-weight: 600;
-    }
+
     &__category-title,
     &__categories {
       grid-column: 1 / span 1;
@@ -90,10 +122,27 @@ export default {
     grid-column: 1 / -1;
   }
 
+  /* Filter */
+
+  h3.filter,
+  .category-btn {
+    font-size: 0.66em;
+  }
+
+  h3.filter{
+    margin-top: 2rem;
+    padding-top: 1em;
+    margin-bottom: 1rem;
+  }
+
+  
+
   @media screen and (min-width: $bp-md) {
     .work {
       grid-template-columns: repeat(3, 1fr);
-
+      ul {
+        grid-column: 2 / -1;
+      }
       li {
         grid-template-columns: 1fr 1fr;
       }
@@ -107,5 +156,15 @@ export default {
     video {
       grid-column: 2 / span 1;
     }
+  }
+
+  .slide-bottom-enter-active,
+  .slide-bottom-leave-active {
+    transition: opacity 0.25s ease-in-out, transform 0.25s ease-in-out;
+  }
+  .slide-bottom-enter,
+  .slide-bottom-leave-to {
+    opacity: 0;
+    transform: translate3d(0, 15px, 0);
   }
 </style>
